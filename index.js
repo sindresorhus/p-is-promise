f8db06e7275ef6fba0d26bb993d9010a7832ec9d
Nicholai Nissen <nicholainissen@gmail.com>
2016-11-26 11:23:15 +0100

    instanceof

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 'use strict';
 module.exports = x => (
-	x !== null &&
-	typeof x === 'object' &&
-	typeof x.then === 'function' &&
-	typeof x.catch === 'function'
-);
+	x instanceof Promise ||
+	(x !== null &&
+		typeof x === 'object' &&
+		typeof x.then === 'function' &&
+		typeof x.catch === 'function'
+	));
