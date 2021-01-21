cacb6022439ba5dff17c1b6b1a7b27e806511f62
dhananjay1994 <34093818+dhananjay1994@users.noreply.github.com>
2021-01-21 14:16:04 +0530

    Update index.test-d.ts

--- index.test-d.ts
+++ index.test-d.ts
@@ -7,5 +7,5 @@ const unicorn: unknown = Promise.resolve('🦄');
 
 if (isPromise(unicorn)) {
 	expectType<Promise<unknown>>(unicorn);
-	expectType<unknown>(await unicorn);
+	expectType<unknown>( unicorn);
 }
